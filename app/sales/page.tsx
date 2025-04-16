7bc7465ec389553b8113bc4b3ad35fdbba166a50
Gustavo Orelio <oreliogustavo@gmail.com>
2025-04-15 22:56:39 -0300

    feat: ensure sales and products page are scrollable

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,15 +1,15 @@
-import { ComboboxOption } from "../_components/ui/combobox";
-import { DataTable } from "../_components/ui/data-table";
-import { getProducts } from "../_data-access/product/get-products";
-import { getSales } from "../_data-access/sale/get-sales";
-import UpsertSaleButton from "./_components/create-sale-button";
-import { saleTableColumns } from "./_components/table-columns";
 import Header, {
   HeaderLeft,
   HeaderRight,
   HeaderSubtitle,
   HeaderTitle,
 } from "../_components/header";
+import { ComboboxOption } from "../_components/ui/combobox";
+import { DataTable } from "../_components/ui/data-table";
+import { getProducts } from "../_data-access/product/get-products";
+import { getSales } from "../_data-access/sale/get-sales";
+import UpsertSaleButton from "./_components/create-sale-button";
+import { saleTableColumns } from "./_components/table-columns";
 
 const SalesPage = async () => {
   const sales = await getSales();
@@ -24,7 +24,7 @@ const SalesPage = async () => {
     productOptions,
   }));
   return (
-    <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
+    <div className="m-8 w-full space-y-8 overflow-auto rounded-lg bg-white p-8">
       <Header>
         <HeaderLeft>
           <HeaderSubtitle>Gestão de Vendas</HeaderSubtitle>
